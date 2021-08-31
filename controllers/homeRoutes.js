3031aeb8ed3d9bd64f8feca314fe55484d7a1c6c
ZahraMertens <82029020+ZahraMertens@users.noreply.github.com>
2021-08-31 13:07:46 +1000

    Add index to api routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,3 +2,10 @@ const router = require('express').Router();
 const { Post, Comment, User } = require('../models');
 const withAuth = require('../utils/auth');
 
+router.get('/', async (req,res) => {
+    try{
+
+    } catch (error){
+        
+    }
+})
