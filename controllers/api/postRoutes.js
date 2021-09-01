5ffc8f3f8cb4af2fae1a19775dbe4b4689eb10c3
ZahraMertens <82029020+ZahraMertens@users.noreply.github.com>
2021-09-01 18:39:37 +1000

    Initialize the update post functionality

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -18,6 +18,35 @@ router.post('/', withAuth, async (req, res) => {
 
     }
 });
+
+router.get('/:id', withAuth, async (req, res) => {
+  try{
+    const postData = await Post.findByPk(req.params.id, {
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+
+    if (!postData) {
+        res.status(200).json({message: 'Create new post'});
+        return
+    }
+
+    const post = postData.get({plain: true});
+
+    res.render('updatePost', {
+        ...post,
+        logged_in: req.session.logged_in
+    });
+
+} catch (error){
+    console.log(error)
+    res.status(500).json({name: error.name, message: error.message})
+}
+});
   
 router.delete('/:id', withAuth, async (req, res) => {
     try {
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -73,8 +73,60 @@ router.get('/login', (req, res) => {
     res.render('login');
 });
 
+router.get('/dashboard', withAuth, async (req, res) => {
+
+    const userId = req.session.user_id;
+    console.log(userId)
+
+    try {
+
+        const userData = await User.findOne({
+            where: {
+                id: userId,
+            }
+        });
+
+        const user = userData.get({plain: true});
+        console.log(user)
+
+        const postData = await Post.findAll({
+            include: [
+                {
+                    model: User,
+                    attributes: ['id', 'username']
+                },
+            ],
+            attributes: ['id', 'title', 'post_content', 'date_created'],
+            where: {
+                user_id: userId,
+            }
+        });
+
+        if (!postData) {
+            res.status(200).json({message: 'errorororor'});
+            return
+        }
+
+        const posts = postData.map((post) => {
+            return post.get({plain: true})
+        });
+
+        console.log(posts)
+
+        res.render('dashboard', {
+            posts,
+            user,
+            logged_in: req.session.logged_in
+        });
+
+    } catch (error) {
+
+        res.status(500).json({name: error.name, message: error.message})
+    }
+})
+
 router.get('/post', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
+ 
     if (!req.session.logged_in) {
       res.redirect('/');
       return;
@@ -84,7 +136,7 @@ router.get('/post', (req, res) => {
 });
 
 router.get('/signup', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
+
     if (req.session.logged_in) {
       res.redirect('/');
       return;
